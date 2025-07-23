9d83f81267461a3e59d03323e671ed87e988a8f5
Robert Wadowski <robert.wadowski@morgenrete.com>
2025-07-23 23:57:37 +0200

    pg8000 used

--- store.py
+++ store.py
@@ -1,11 +1,10 @@
-import psycopg2
-
+import pg8000
 
 def logic(v):
-    sql = """INSERT INTO value_table (value) VALUES (%s) RETURNING id"""
+    sql = "INSERT INTO value_table (value) VALUES (%s) RETURNING id"
     conn = None
     try:
-        conn = psycopg2.connect(
+        conn = pg8000.connect(
             host="localhost",
             database="mcep",
             user="mcep",
@@ -16,15 +15,9 @@ def logic(v):
         value_id = cur.fetchone()[0]
         conn.commit()
         cur.close()
-        r = {
-            "z": value_id
-        }
-        return r
-    except (Exception, psycopg2.DatabaseError) as error:
-        r = {
-            "error": str(error)
-        }
-        return r
+        return {"z": value_id}
+    except Exception as error:
+        return {"error": str(error)}
     finally:
         if conn is not None:
             conn.close()
